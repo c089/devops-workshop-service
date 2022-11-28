e30a8946964a9c0a70b705606126b50c10c8fa9f
Raimo Radczewski <hello@craftswerk.io>
2022-11-28 15:24:03 +0100

    Fail fast when validating prereqs, but not too fast so it's annoying

--- cluster/prerequisites.sh
+++ cluster/prerequisites.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-set -ex
+set -e +x
+
+SHOULD_EXIT_BECAUSE_OF_MISSING_DEPENDENCY=0
 
 command_exists() {
     local COMMAND="$1"
-    if ! command -v "$COMMAND"; then
-	echo "Install '${COMMAND}'"
-	exit 1
+    local INSTALL_GUIDE="$2"
+    if ! command -v "$COMMAND" > /dev/null; then
+	echo "Install '${COMMAND}' ( see ${INSTALL_GUIDE} )"
+	SHOULD_EXIT_BECAUSE_OF_MISSING_DEPENDENCY=1
     fi
 }
 
@@ -14,25 +17,32 @@ has_helm_repo() {
     local NAME="$1"
     local URL="$2"
 	set +e
-	helm repo list -o json | jq -e ".[] | select(.name == \"${NAME}\" and .url == \"${URL}\")"
+	helm repo list -o json | jq -e ".[] | select(.name == \"${NAME}\" and .url == \"${URL}\")" > /dev/null
     if [ $? -ne 0 ]
     then
-        echo "Run 'helm repo add ${NAME} ${URL}' to add the missing repo"
-        exit 1
+        echo "helm repo add ${NAME} ${URL}"
+        SHOULD_EXIT_BECAUSE_OF_MISSING_DEPENDENCY=1
     fi
 	set -e
 }
 
-command_exists "kubectl"
-command_exists "jq"
-command_exists "k3d"
-command_exists "helm"
-command_exists "mkcert"
-command_exists "argo"
-command_exists "argocd"
+command_exists "kubectl" "https://kubernetes.io/docs/tasks/tools/"
+command_exists "jq" "https://stedolan.github.io/jq/"
+command_exists "k3d" "https://k3d.io/v5.4.6/#installation"
+command_exists "helm" "https://helm.sh/docs/intro/install/"
+command_exists "mkcert" "https://github.com/FiloSottile/mkcert"
+command_exists "argo" "https://argoproj.github.io/argo-workflows/quick-start/#install-the-argo-workflows-cli"
+command_exists "argocd" "https://argo-cd.readthedocs.io/en/stable/getting_started/#2-download-argo-cd-cli"
 
 has_helm_repo "traefik" "https://traefik.github.io/charts"
 has_helm_repo "prometheus-community" "https://prometheus-community.github.io/helm-charts"
 has_helm_repo "grafana" "https://grafana.github.io/helm-charts"
 has_helm_repo "argo" "https://argoproj.github.io/argo-helm"
 has_helm_repo "gitea-charts" "https://dl.gitea.io/charts/"
+
+if [ "${SHOULD_EXIT_BECAUSE_OF_MISSING_DEPENDENCY}" -ne 0 ];
+then
+    echo ""
+    echo "You need to install the dependencies listed above to continue."
+    exit 1
+fi
