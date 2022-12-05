88a3784fa226682582f040662a8fee633329e70f
Christoph Neuroth <christoph.neuroth@gmail.com>
2022-12-05 14:02:30 +0100

    Split into multiple repositories

--- cluster/argocd-login.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-argocd \
-  login \
-  argocd.k3d.localhost \
-  --username admin \
-  --password $(kubectl get secret -n argocd argocd-initial-admin-secret -o json|jq .data.password -j | base64 -d)
--- cluster/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-CLUSTER_DIR=$(dirname "$0")
-
-cd "${CLUSTER_DIR}/test"
-shellspec "$@"
