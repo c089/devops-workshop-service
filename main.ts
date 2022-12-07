fdbd73ec8fcdc4e5aef1792526557c8023288dfa
Raimo Radczewski <hello@craftswerk.io>
2022-12-07 14:38:16 +0100

    Add hostname to request

--- main.ts
+++ main.ts
@@ -3,14 +3,14 @@ async function serveHttp(conn: Deno.Conn) {
   for await (const requestEvent of httpConn) {
     console.log(`Received a friendly request, responding with a greeting.`);
     requestEvent.respondWith(
-      new Response("Hello lovelies!", { status: 200 }),
+      new Response("Hello lovelies! "+Deno.env.get("HOSTNAME"), { status: 200 }),
     );
   }
 }
 
 if (import.meta.main) {
   const server = Deno.listen({ port: 8080 });
-  console.log(`Hello service waiting for friendly greeting requests.`);
+  console.log(`Hello service waiting for friendly greeting requests. ${Deno.env.get("HOSTNAME")}`);
 
   for await (const conn of server) {
     serveHttp(conn);
